95c8f69372451ab5b17caf73ca195309d897500c
Joseph Vincent <josephvincentlimbaroc@gmail.com>
2024-02-13 16:07:38 +0800

    Fix error in child

--- resources/views/clients/client-children.blade.php
+++ resources/views/clients/client-children.blade.php
@@ -133,7 +133,7 @@
                                     Age<span class="text-red-500">*</span>
                                 </label>
                                 <div>
-                                    <input type="number" name="age" id="child_age"
+                                    <input type="number" name="age" id="child_age" required
                                         class="block w-full rounded-md border-0 px-3 py-1.5
                                         text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
                                         placeholder:text-gray-400 focus:ring-2 focus:ring-inset
@@ -163,7 +163,7 @@
                                     Educational attainment<span class="text-red-500">*</span>
                                 </label>
                                 <div>
-                                    <select id="educ_level" name="educ_level"
+                                    <select id="educ_level" name="educ_level" required
                                         class="mt-1 block w-full rounded-md border-0 px-2 py-1.5 pr-10
                                         text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2
                                         focus:ring-violet-600 sm:text-sm sm:leading-6">
