2d6edb175234ddefe3b9c52f0e2f18e65de6f108
Joseph Vincent <josephvincentlimbaroc@gmail.com>
2024-02-21 15:38:17 +0800

    Fix Error

--- public/scripts/client-update.js
+++ public/scripts/client-update.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
     $("#birthdate").flatpickr({
         allowInput: true,
     });
+
+    $('#birthdate').change(function () {
+        var dob = $(this).val();
+        var birthDate = new Date(dob);
+        var currentDate = new Date();
+        var age = currentDate.getFullYear() - birthDate.getFullYear();
+        if (currentDate.getMonth() < birthDate.getMonth() || (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        $('#age').val(age);
+    });
 });
