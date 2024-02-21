b29880fa72964dfaddd8930995b40b3641cba006
Joseph Vincent <josephvincentlimbaroc@gmail.com>
2024-02-21 13:08:10 +0800

    Fix attachment

--- resources/views/cases/case-log.blade.php
+++ resources/views/cases/case-log.blade.php
@@ -224,26 +224,23 @@
                                                             </option>
                                                         </select>
                                                     </div>
-                                                    @if ($caselog->referredBy->agency->agency_name == $caselog->referralAgency->agency_name)
-                                                        <div class="flex mt-1">
-                                                            <label for="fileInput"
-                                                                class="group -my-2 -ml-2 inline-flex items-center rounded-full px-3 py-2 text-left text-gray-400 cursor-pointer">
-                                                                <svg class="-ml-1 mr-2 h-5 w-5 group-hover:text-gray-500"
-                                                                    viewBox="0 0 20 20" fill="currentColor"
-                                                                    aria-hidden="true">
-                                                                    <path fill-rule="evenodd"
-                                                                        d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z"
-                                                                        clip-rule="evenodd" />
-                                                                </svg>
-                                                                <input type="file" name="attachments[]" id="fileInput"
-                                                                    class="hidden" multiple
-                                                                    onchange="displayFileNames(this)">
-                                                                <span
-                                                                    class="text-sm italic text-gray-500 group-hover:text-gray-600"
-                                                                    id="fileNames">Attach a file</span>
-                                                            </label>
-                                                        </div>
-                                                    @endif
+                                                    <div class="flex mt-1">
+                                                        <label for="fileInput"
+                                                            class="group -my-2 -ml-2 inline-flex items-center rounded-full px-3 py-2 text-left text-gray-400 cursor-pointer">
+                                                            <svg class="-ml-1 mr-2 h-5 w-5 group-hover:text-gray-500"
+                                                                viewBox="0 0 20 20" fill="currentColor"
+                                                                aria-hidden="true">
+                                                                <path fill-rule="evenodd"
+                                                                    d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z"
+                                                                    clip-rule="evenodd" />
+                                                            </svg>
+                                                            <input type="file" name="attachments[]" id="fileInput"
+                                                                class="hidden" multiple onchange="displayFileNames(this)">
+                                                            <span
+                                                                class="text-sm italic text-gray-500 group-hover:text-gray-600"
+                                                                id="fileNames">Attach a file</span>
+                                                        </label>
+                                                    </div>
                                                 </div>
                                                 <div class="mt-3 flex items-center justify-end">
                                                     <button type="submit"
